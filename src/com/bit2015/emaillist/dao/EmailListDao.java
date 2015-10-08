5f1e6c561118fa14bdc865fc4c71a4907d39f9ea
bit-user <bit-user@222.106.22.80>
2015-10-08 13:13:43 +0900

    DAO refactoring

--- src/com/bit2015/emaillist/dao/EmailListDao.java
+++ src/com/bit2015/emaillist/dao/EmailListDao.java
@@ -12,37 +12,49 @@ import java.util.List;
 import com.bit2015.emaillist.vo.EmailListVo;
 
 public class EmailListDao {
-	
-	public void insert( EmailListVo vo ) {
+	private Connection getConnection() throws SQLException {
+		
+		Connection connection = null;
+		
 		try {
 			//1.드라이버 로딩
 			Class.forName( "oracle.jdbc.driver.OracleDriver" );
-			
+		
 			//2.커넥션 만들기(ORACLE DB)
 			String dbURL  = "jdbc:oracle:thin:@localhost:1521:xe";
-			Connection connection = DriverManager.getConnection( dbURL, "webdb", "webdb" );
+			connection = DriverManager.getConnection( dbURL, "webdb", "webdb" );
 			
-			//3. Statement 준비
+		} catch( ClassNotFoundException ex ){
+			System.out.println( "드라이버 로딩 실패-" + ex );
+		} 
+		
+		return connection;
+	}
+	
+	public void insert( EmailListVo vo ) {
+		try {
+			//1. Connection 가져오기
+			Connection connection = getConnection();
+			
+			//2. Statement 준비
 			String sql = 
 				" insert" +
 				" into email_list" + 
 				" values( email_list_no_seq.nextval, ?, ?, ? )";
 			PreparedStatement pstmt = connection.prepareStatement( sql );
 			
-			//4. binding
+			//3. binding
 			pstmt.setString( 1, vo.getFirstName() );
 			pstmt.setString( 2, vo.getLastName() );
 			pstmt.setString( 3, vo.getEmail() );
 			
-			//5. query 실행
+			//4. query 실행
 			pstmt.executeUpdate();
 			
-			//6. 자원정리
+			//5. 자원정리
 			pstmt.close();
 			connection.close();
 			
-		} catch( ClassNotFoundException ex ) {
-			System.out.println( "드라이버 로딩 실패-" + ex  );
 		} catch( SQLException ex ) {
 			System.out.println( "SQL 오류-" + ex );
 		}
@@ -52,21 +64,17 @@ public class EmailListDao {
 		List<EmailListVo> list = new ArrayList<EmailListVo>();
 		
 		try {
-			//1.드라이버 로딩
-			Class.forName( "oracle.jdbc.driver.OracleDriver" );
+			//1. 커넥션 만들기(ORACLE DB)
+			Connection connection = getConnection();
 			
-			//2. 커넥션 만들기(ORACLE DB)
-			String dbURL  = "jdbc:oracle:thin:@localhost:1521:xe";
-			Connection connection = DriverManager.getConnection( dbURL, "webdb", "webdb" );
-			
-			//3. Statement 생성
+			//2. Statement 생성
 			Statement stmt = connection.createStatement();
 			
-			//4. SQL문 실행
+			//3. SQL문 실행
 			String sql = "select * from email_list order by no desc";
 			ResultSet rs = stmt.executeQuery( sql );
 			
-			//5. row 가져오기
+			//4. row 가져오기
 			while( rs.next() ) {
 				Long no = rs.getLong( 1 );
 				String firstName = rs.getString( 2 );
@@ -87,8 +95,6 @@ public class EmailListDao {
 			stmt.close();
 			connection.close();
 			
-		} catch( ClassNotFoundException ex ) {
-			System.out.println( "드라이버 로딩 실패-" + ex  );
 		} catch( SQLException ex ) {
 			System.out.println( "SQL 오류-" + ex );
 		}
